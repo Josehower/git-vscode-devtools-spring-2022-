36bf21bc7195f617d688699f80a8a28e933d0b07
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2022-05-12 10:16:48 +0200

    Add index.js logic

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log('#####################');
+console.log('#####################');
+console.log('#####################');
+
+const animal = [
+  { id: 1, name: 'turtle', nickName: 'dodo', age: 100 },
+  { id: 2, name: 'turtle', nickName: 'dodo', age: 100 },
+  { id: 3, name: 'turtle', nickName: 'dodo', age: 100 },
+  { id: 4, name: 'turtle', nickName: 'dodo', age: 100 },
+  { id: 5, name: 'turtle', nickName: 'dodo', age: 100 },
+  { id: 6, name: 'turtle', nickName: 'dodo', age: 100 },
+  { id: 7, name: 'turtle', nickName: 'dodo', age: 100 },
+  { id: 8, name: 'turtle', nickName: 'dodo', age: 100 },
+  { id: 9, name: 'turtle', nickName: 'dodo', age: 100 },
+  { id: 10, name: 'turtle', nickName: 'dodo', age: 100 },
+];
